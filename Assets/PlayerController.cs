0da88ab5c207e05cd9a8c230d9ac698d7ef7d223
Rio Ikeda <kriorio.ks714@gmail.com>
2019-12-20 14:15:20 +0900

    [add]プレイヤーを動かす

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("down"))
+        {
+            transform.position -= transform.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("right"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+    }
+}
